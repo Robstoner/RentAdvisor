130b32584ed9fe11187756ad5f3c1caaa5e4e7d8
Robert Schmidt <robert.schmidt@nevexo.ro>
2024-11-24 12:56:26 +0200

    Initial setup

---
+++ RentAdvisor.Server/Database/AppDatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RentAdvisor.Server.Models.Entities;
+
+namespace RentAdvisor.Server.Database
+{
+    public class AppDatabaseContext : IdentityDbContext<User>
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppDatabaseContext() : base()
+        {
+        }
+        public AppDatabaseContext(DbContextOptions<AppDatabaseContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ RentAdvisor.Server/Models/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RentAdvisor.Server.Models.Entities
+{
+    public class User : IdentityUser
+    {
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime LastUpdatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime lastLogin { get; set; } = DateTime.UtcNow;
+    }
+}
