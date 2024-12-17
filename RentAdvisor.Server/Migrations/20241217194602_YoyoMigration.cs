07b76c070267aa3ffb7201dca0f6d5cac9fc4c80
ursakee <ovidiufierascu@gmail.com>
2024-12-17 23:41:02 +0200

    update

---
+++ RentAdvisor.Server/Migrations/20241217194602_YoyoMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentAdvisor.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class YoyoMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Score",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Score",
+                table: "AspNetUsers");
+        }
+    }
+}
